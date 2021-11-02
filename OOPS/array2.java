53e5a84e7d6480517452802bd9d4db22e6d83ee4
deeksha-678 <deeksha8480@gmail.com>
2021-11-02 16:09:44 +0530

    comm

---
+++ OOPS/array2.java
@@ -0,0 +1,11 @@
+class array2
+{
+    public static void main(String[] args)
+    {
+    int array2[]={38,40,42};
+    char []size={'s','m','l'};
+    String[] enames={"ram","sam","fam"};
+    System.out.println(array2.length);
+    System.out.println(size.length);
+    System.out.println(enames.length);
+}}
