7b270f1291ca3147fa22f926e870448af549e95d
deeksha-678 <deeksha8480@gmail.com>
2021-11-16 17:09:23 +0530

    commit

---
+++ OOPS/evenfive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class evenfive
+{
+    public static void main(String[] args)
+    {
+       
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a numb:");
+        int n=sc.nextInt();
+        System.out.println("even numbers are:"+n);
+        for(int i=0;i<=n;i=i+2)
+        {
+            
+                System.out.println(i+"");
+            
+        }}}
---
+++ OOPS/multsix.java
@@ -0,0 +1,12 @@
+class multiple
+{
+    public static void main(String[] args)
+    {
+        int i=6;
+        while(i<=60)
+        {
+            System.out.println(i);
+            i=i+6;
+        }
+    }
+}
