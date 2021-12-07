b01c74cb907aaf4c37f4f3b1fec1a8b795b419f9
deeksha-678 <deeksha8480@gmail.com>
2021-12-07 14:24:18 +0530

    commit

---
+++ OOPS/EH2.java
@@ -0,0 +1,15 @@
+class Example{
+  public static void main(String args[]){
+    int x,y,z;
+    try{
+      x = 5;
+      y = 0;
+      z = x / y;
+      System.out.println("The output for z cannot be executed.");
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("Expression throws divide by 0 exceptions");
+    }
+  }
+}
